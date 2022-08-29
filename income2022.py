dc77341a9141c172ce24be9c5d7977251d3ebfd8
ajuveriya <59317931+ajuveriya@users.noreply.github.com>
2022-08-29 20:08:23 +0530

    Update income2022.py

--- income2022.py
+++ income2022.py
@@ -18,5 +18,22 @@ if st.sidebar.checkbox("Show income data"):
     df = pd.DataFrame(
     [['NAME', 'INCOME', 'EXPENDITURE','BALANCE'], ['QADEER PASHA', 250630,242644,7986], ['KAREEMUDDIN',60370 ,47744,12626], ['ARJ',73000 ,0,73000],['TOTAL',384000,	290388,73000]])
     st.table(df)
+st.sidebar.subheader("Month Wise Income and Expenditure 2022")
+plot_types = st.sidebar.multiselect("Select the Month:", 
+                                    ('January', 'February', 'March', 'April', 'May', 'June','July',"August"))
+if 'January' in plot_types:
+    st.subheader("January 2022")
+    st.dataframe(school_df)
+     df = pd.DataFrame(
+    [['NAME', 'INCOME', 'EXPENDITURE','BALANCE'], ['QADEER PASHA', 250630,242644,7986], ['KAREEMUDDIN',60370 ,47744,12626], ['ARJ',73000 ,0,73000],['TOTAL',384000,	290388,73000]])
 
+    st.table(df)
+
+if 'February' in plot_types:
+    st.subheader("February 2022")
+    st.dataframe(school_df)
+     df = pd.DataFrame(
+    [['NAME', 'INCOME', 'EXPENDITURE','BALANCE'], ['QADEER PASHA', 250630,242644,7986], ['KAREEMUDDIN',60370 ,47744,12626], ['ARJ',73000 ,0,73000],['TOTAL',384000,	290388,73000]])
+
+    st.table(df)
 
